0a8919b33920ba8ea26409b106bacd2fb8fe52e8
Toheed <toheedjamaal9@gmail.com>
2023-04-24 13:22:54 +0530

    completed

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', false);
 const cors = require("cors");
-const serverless = require('serverless-http');
-const router = express.Router();
+
+const fs = require("fs");
+const rawData = fs.readFileSync("./sample_data.json");
+// const data = JSON.parse(rawData);
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Models
-const User = require("../models/user");
+const User = require("./models/user");
+
+// data.forEach((item) => {
+//   const newData = new User({
+//     id: item.id,
+//     first_name: item.first_name,
+//     last_name: item.last_name,
+//     email: item.email,
+//     gender: item.gender,
+//     income: item.income,
+//     city: item.city,
+//     car: item.car,
+//     quote: item.quote,
+//     phone_price: item.phone_price,
+//   });
+
+//   newData.save((err) => {
+//     console.log("users added")
+//       if (err) {
+//           console.error(err);
+//       }
+//   });
+// });
 
 let data = [];
 
-router.get("/users", async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
     data = await User.find();
     res.json(data);
@@ -19,7 +48,7 @@ router.get("/users", async (req, res) => {
   }
 });
 
-router.get("/users/income-bmw-mercedes", (req, res) => {
+app.get("/users/income-bmw-mercedes", (req, res) => {
   const filteredUsers = data.filter((user) => {
     const income = parseFloat(user.income.replace("$", ""));
     return income < 5 && (user.car === "BMW" || user.car === "Mercedes");
@@ -27,14 +56,14 @@ router.get("/users/income-bmw-mercedes", (req, res) => {
   res.json(filteredUsers);
 });
 
-router.get("/users/male-phone-price", (req, res) => {
+app.get("/users/male-phone-price", (req, res) => {
   const filteredUsers = data.filter((user) => {
     return user.gender === "Male" && user.phone_price > 10000;
   });
   res.json(filteredUsers);
 });
 
-router.get("/users/last-name-quote-email", (req, res) => {
+app.get("/users/last-name-quote-email", (req, res) => {
   const filteredUsers = data.filter((user) => {
     return (
       user.last_name.toLowerCase().startsWith("m") &&
@@ -45,7 +74,7 @@ router.get("/users/last-name-quote-email", (req, res) => {
   res.json(filteredUsers);
 });
 
-router.get("/users/car-brand-email", (req, res) => {
+app.get("/users/car-brand-email", (req, res) => {
   const filteredUsers = data.filter((user) => {
     const carBrands = ["BMW", "Mercedes", "Audi"];
     const regex = /\d/;
@@ -54,7 +83,7 @@ router.get("/users/car-brand-email", (req, res) => {
   res.json(filteredUsers);
 });
 
-router.get("/users/top-cities", (req, res) => {
+app.get("/users/top-cities", (req, res) => {
   const cityCounts = {};
   const cityIncomes = {};
   data.forEach((user) => {
@@ -81,7 +110,7 @@ router.get("/users/top-cities", (req, res) => {
 });
 
 mongoose.connect(
-  process.env.MONGODB_URI,
+  "mongodb://localhost/mobilicis-DB",
   {
     useUnifiedTopology: true,
     useNewUrlParser: true,
@@ -91,12 +120,6 @@ mongoose.connect(
   }
 );
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use('/.netlify/functions/server', router);
-
-module.exports.handler = serverless(app, {
-  requestTimeout: 30000
+app.listen(3001, () => {
+  console.log("Server is live");
 });
